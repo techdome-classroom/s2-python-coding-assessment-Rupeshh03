9fa2da6202e7d40ea1786e54a3e1b09c96f7a3f7
Rupesh Pradhan <rupeshpradhan869@gmail.com>
2024-10-07 14:32:01 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -4,7 +4,23 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        pass
+        # Dictionary to hold matching pairs
+        matching = {')': '(', '}': '{', ']': '['}
+        
+        # Stack to hold the opening brackets
+        stack = []
+        
+        # Traverse through each character in the string
+        for char in s:
+            if char in matching.values():  # If it's an opening bracket
+                stack.append(char)
+            elif char in matching:  # If it's a closing bracket
+                # If the stack is empty or the top doesn't match
+                if not stack or stack.pop() != matching[char]:
+                    return False
+        
+        # If the stack is empty, all brackets were properly matched
+        return not stack
 
 
 
